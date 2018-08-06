f332f16f4d1e782b7872175dba9108121334073c
HV Sharma <hvsharma63@gmail.com>
2018-08-07 00:09:44 +0530

    Cleaned version of Single.php Added!

--- single.php
+++ single.php
@@ -2,19 +2,10 @@
     require_once "config.php";
         
         $data = $_SESSION['data'];
-        //echo $_GET['id'];
         $id = $_GET['id'];
-        //echo "<br>";
-        $count=0;$notfound=0;
-        // foreach($data['albums'][0]['photos'][$i]['images'][0] as $current_album)
-        // {
-        //     if($current_album['id'] == $id){ //upto which digit do I've to set the limit?
-        //         echo $current_album['id'];        
-        //     }
-        // }
-        $albums = count($data['albums']);
-        echo "Total Albums: " . $albums;
-        echo "<br>";
+        $albums = count($data['albums']); 
+
+        // The following loop will check whether the fetched id from dashboard is available in the json file or not!
         for($i=0;$i<$albums;$i++){
             if($id == $data['albums'][$i]['id']){
                 $found = 1;
@@ -23,83 +14,38 @@
             }
             $found = 0;
         }
-        if($found == 1){
-            
-            
-                // for($j=0;$j<count($data['albums'][$i]);$j++){
-                    
-                    for($k=0;$k<count($data['albums'][$correctIdNumber]['photos']);$k++){
-                        
-                        // for($l=0;$l<count($data['albums'][$i]['photos'][$k]);$l++){
-                            
-                            // for($m=0;$m<count($data['albums'][$i]['photos'][$k]['images']);$m++){
-                            
-                                // for($n=0;$n<count($data['albums'][$i]['photos'][$k]['images'][$m]);$n++){
-                            ?>
-                            <img src="<?php echo $data['albums'][$correctIdNumber]['photos'][$k]['images'][0]['source']; ?>" height="300px" width="250px" alt="" srcset="">
-                            <?php echo "<br>";
-                                // }
-                            // }
-                        }
-                    // }
-                // }
-                
-        }
-        else{
-            echo "Nothing Found!";
-        }
+
+        // Just for the Reference!
         // echo count($data['albums'][0]); // 6
         // echo count($data['albums'][0]['photos']); // 5
         // echo count($data['albums'][0]['photos'][0]); // 2
         // echo count($data['albums'][0]['photos'][0]['images'][0]); //3
         // echo count($data['albums'][0]['photos'][0]['images']); //9
-        // for($i=0;$i<$albums;$i++){
-            
-        //     // for($j=0;$j<count($data['albums'][$i]);$j++){
-                
-        //         for($k=0;$k<count($data['albums'][$i]['photos']);$k++){
-                    
-        //             // for($l=0;$l<count($data['albums'][$i]['photos'][$k]);$l++){
-                        
-        //                 // for($m=0;$m<count($data['albums'][$i]['photos'][$k]['images']);$m++){
-                        
-        //                     // for($n=0;$n<count($data['albums'][$i]['photos'][$k]['images'][$m]);$n++){
-        //                 ?>
-        <!-- //                 <img src="<?php echo $data['albums'][$i]['photos'][$k]['images'][4]['source']; ?>" alt="" srcset=""> -->
-        <!-- //                 <?php echo "<br>"; ?> -->
-        //                     // }
-        //                 // }
-        //             }
-        //         // }
-        //     // }
-        // }
         //var_dump($data['albums'][0]['photos'][0]['webp_images'][0]['source']); 
-        <?php
-        echo "<pre>";
-        var_dump($data['albums']);
-        exit;
+        
 ?>
 <?php include_once "./include/header.php" ?>
     <div class="container">
         <?php
-            $i=0;
-            foreach($data['albums'] as $current_album)
-            {
+            if($found == 1){    
+                for($k=0;$k<count($data['albums'][$correctIdNumber]['photos']);$k++){    
         ?>
-                <div class="col-md-12">
+                <div class="col-md-4">
                     <div class="panel panel-default">
                         <div class="panel-heading">
-                            <h3 class="panel-title"> <?php print_r($current_album['albums']['name'][$id]); ?> </h3>
+                            <h3 class="panel-title">Image</h3>
                         </div>
                         <div class="panel-body">
-                        
+                            <img src="<?php echo $data['albums'][$correctIdNumber]['photos'][$k]['images'][0]['source']; ?>" height="300px" width="300px" alt="" srcset="">
                         </div>
                     </div>
                 </div>
         <?php
+                }
+            }
+            else{
+                echo "Sorry, Wrong ID! And Hence No Album Found!";
             }
         ?>
-
-        
     </div>
 <?php include_once "./include/footer.php" ?>
